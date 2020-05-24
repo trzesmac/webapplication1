01e17ea9346fd6c86a9b9008e52d4e8021b4a4b5
trzesmac <igort@vp.pl>
2020-05-24 23:08:03 +0200

    Update for test commit

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -20,7 +20,6 @@ namespace WebApplication1
       }
 
       public IConfiguration Configuration { get; }
-      //komentarz
       // This method gets called by the runtime. Use this method to add services to the container.
       public void ConfigureServices(IServiceCollection services)
       {
